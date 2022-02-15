2d3441d947ed2cdb066758cdd5a708180691ef61
Komron777 <komronfayziboev@gmail.com>
2022-02-15 19:41:09 +0500

    fixed another bug

--- hw1.go
+++ hw1.go
@@ -3,7 +3,6 @@ package homework1
 import "github.com/kyokomi/emoji"
 
 func HelloWithEmoji () string{
-	world_map := ":world_map:"
-	result := emoji.Sprint("Hello " + world_map + "!")
-	return result
+	world := ":world_map:"
+	return emoji.Sprint("Hello ",world,"!")
 }
